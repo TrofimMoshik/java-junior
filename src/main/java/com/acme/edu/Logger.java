e3013bf6f4af9b9a238053a98d68810af39c7041
TrofimMoshik <pp1834@yandex.ru>
2018-03-05 22:07:08 +0300

    Refactoring to OOAD ver. 2.

---
+++ src/main/java/com/acme/edu/Logger.java
@@ -0,0 +1,48 @@
+package com.acme.edu;
+
+/**
+ * Logger-facade.
+ */
+public class Logger {
+    public static Controller controller = new Controller();
+
+    public static void log(int message) {
+        controller.add(message);
+    }
+
+    public static void log (String message) {
+        controller.add(message);
+    }
+
+    public static void log(byte message) {
+        controller.add(message);
+    }
+
+    public static void log(char message) {
+        controller.add(message);
+    }
+
+    public static void log(boolean message) {
+        controller.add(message);
+    }
+
+    public static void log(Object message) {
+        controller.add(message);
+    }
+
+    public static void log(int... message) {
+        controller.add(message);
+    }
+
+    public static void log(int[][] message) {
+        controller.add(message);
+    }
+
+    public static void log(int[][][][] message) {
+        controller.add(message);
+    }
+
+    public static void log(String... message) {
+        controller.add(message);
+    }
+}
